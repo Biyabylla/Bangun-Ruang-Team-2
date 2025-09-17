a1c8018eea876efc6ea9732311d4210b0061a3d5
Biyabylla <ardinanabila35@gmail.com>
2025-09-17 10:17:43 +0700

    class alas persegi

---
+++ src/bangunruangteam2/alasPersegiAhid.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bangunruangteam2;
+
+/**
+ *
+ * @author Nabila
+ */
+public class alasPersegiAhid {
+    
+}
