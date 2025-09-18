ec7b85351fba0d8e0715469829040bd2b7ee1be7
Ilham Muzaki <Ilham Muzaki@LAPTOP-IGGPRTH3>
2025-09-18 15:14:34 +0700

    kerucut

--- src/bangunruangteam2/kerucutIlham.java
+++ src/bangunruangteam2/kerucutIlham.java
@@ -9,5 +9,28 @@ package bangunruangteam2;
  * @author Nabila
  */
 public class kerucutIlham {
+    private double jarijari;
+    private double tinggi;
+    
+    public void setjarijari(double jarijari){
+        this.jarijari = jarijari;
+    }
+     public void settinggi(double tinggi){
+         this.tinggi = tinggi;
+     }
+     public double getjarijari(){
+         return this.jarijari;
+     }
+     public double gettinggi(){
+         return this.tinggi;
+     }
+     public double getvolume(){
+         return (1.0/3.0)*Math.PI*jarijari*jarijari*tinggi;            
+     }
+     
+     public double getluaspermukaan(){
+         double s = Math.sqrt((jarijari*jarijari)+(tinggi*tinggi)); // garis pelukis
+         return Math.PI* jarijari * (jarijari+s);
+     }
     
 }
