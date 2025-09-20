3726c5622c795f29bfc1ba3597957e791de53ab7
Biyabylla <ardinanabila35@gmail.com>
2025-09-20 21:44:43 +0700

    Update BnangunRuangTeam2

--- src/bangunruangteam2/BangunRuangTeam2.java
+++ src/bangunruangteam2/BangunRuangTeam2.java
@@ -14,5 +14,16 @@ public class BangunRuangTeam2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        kerucutIlham k2 = new kerucutIlham();
+        
+        k2.setjarijari(7);
+        k2.settinggi(10);
+        
+        System.out.println("volume kerucut="+k2.getvolume());
+        System.out.println("luas permukaan kerucut="+k2.getluaspermukaan());
     }
+    
+ 
 }
+
+    
